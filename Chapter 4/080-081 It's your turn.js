51baa9d095296c28cac8db9d2d6dffd729563bb9
mechaniccoder <yuseunghwan94@gmail.com>
2022-07-09 13:52:35 +0900

    feat: extract business rule

--- Chapter 4/080-081 It's your turn.js
+++ Chapter 4/080-081 It's your turn.js
@@ -1,15 +1,18 @@
 // Page 80 It's your turn
 
 function update_shipping_icons() {
-  var buy_buttons = get_buy_buttons_dom();
-  for(var i = 0; i < buy_buttons.length; i++) {
+  var buy_buttons = get_buy_buttons_dom(); // action
+  for (var i = 0; i < buy_buttons.length; i++) {
     var button = buy_buttons[i];
     var item = button.item;
-    if(item.price + shopping_cart_total >= 20)
+    if (canGetFreeShipiing(item.price, shopping_cart_total))
       button.show_free_shipping_icon();
-    else
-      button.hide_free_shipping_icon();
+    else button.hide_free_shipping_icon();
   }
+}
+
+function canGetFreeShipiing(price, amount) {
+  return price + amount >= 20;
 }
 
 // Page 81 Answer
@@ -18,15 +21,13 @@ function update_shipping_icons() {
 
 function update_shipping_icons() {
   var buy_buttons = get_buy_buttons_dom();
-  for(var i = 0; i < buy_buttons.length; i++) {
+  for (var i = 0; i < buy_buttons.length; i++) {
     var button = buy_buttons[i];
     var item = button.item;
-    if(gets_free_shipping(item.price))
-      button.show_free_shipping_icon();
-    else
-      button.hide_free_shipping_icon();
-    }
+    if (gets_free_shipping(item.price)) button.show_free_shipping_icon();
+    else button.hide_free_shipping_icon();
   }
+}
 
 function gets_free_shipping(item_price) {
   return item_price + shopping_cart_total >= 20;
