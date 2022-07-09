0e2e68b1f2f9f9d577ed213d2b3f65cb45792344
mechaniccoder <yuseunghwan94@gmail.com>
2022-07-09 13:37:32 +0900

    docs: what is side effect

--- Chapter 4/066 Functiona have inputs and outputs.js
+++ Chapter 4/066 Functiona have inputs and outputs.js
@@ -1,7 +1,11 @@
 var total = 0;
 
 function add_to_total(amount) {
-  console.log("Old total: " + total);
-  total += amount;
-  return total;
+  console.log("Old total: " + total); // implicit output
+  total += amount; // implicit output
+  return total; // explicit output
 }
+
+/**
+ * side effect === implicit ouptut, implicit input
+ */
