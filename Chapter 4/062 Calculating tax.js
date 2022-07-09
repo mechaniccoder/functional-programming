70ac6e88e687a43e8a5b6a1b47c5a1d8e8802bd3
mechaniccoder <yuseunghwan94@gmail.com>
2022-07-09 13:41:11 +0900

    refactor: remove side effect

--- Chapter 4/062 Calculating tax.js
+++ Chapter 4/062 Calculating tax.js
@@ -1,14 +1,19 @@
 function update_tax_dom() {
-  set_tax_dom(shopping_cart_total * 0.10);
+  set_tax_dom(shopping_cart_total * 0.1);
 }
 
 function calc_cart_total() {
-  shopping_cart_total = 0;
-  for(var i = 0; i < shopping_cart.length; i++) {
-    var item = shopping_cart[i];
-    shopping_cart_total += item.price;
-  }
+  calculateTotal();
   set_cart_total_dom();
   update_shipping_icons();
   update_tax_dom();
+}
+
+function calculateTotal(cart) {
+  let total = 0;
+  for (var i = 0; i < cart.length; i++) {
+    var item = cart[i];
+    total += item.price;
+  }
+  return total;
 }
