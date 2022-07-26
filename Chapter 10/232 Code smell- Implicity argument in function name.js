6ec1f2dae6d7c079558710517ad5034f6a20eb40
mechaniccoder <yuseunghwan94@gmail.com>
2022-07-26 23:24:20 +0900

    Make field first class

--- Chapter 10/232 Code smell- Implicity argument in function name.js
+++ Chapter 10/232 Code smell- Implicity argument in function name.js
@@ -1,27 +1,27 @@
 function setPriceByName(cart, name, price) {
   var item = cart[name];
-  var newItem = objectSet(item, 'price', price);
+  var newItem = objectSet(item, "price", price);
   var newCart = objectSet(cart, name, newItem);
   return newCart;
 }
 
 function setShippingByName(cart, name, ship) {
   var item = cart[name];
-  var newItem = objectSet(item, 'shipping', ship);
+  var newItem = objectSet(item, "shipping", ship);
   var newCart = objectSet(cart, name, newItem);
   return newCart;
 }
 
 function setQuantityByName(cart, name, quant) {
   var item = cart[name];
-  var newItem = objectSet(item, 'quantity', quant);
+  var newItem = objectSet(item, "quantity", quant);
   var newCart = objectSet(cart, name, newItem);
   return newCart;
 }
 
 function setTaxByName(cart, name, tax) {
   var item = cart[name];
-  var newItem = objectSet(item, 'tax', tax);
+  var newItem = objectSet(item, "tax", tax);
   var newCart = objectSet(cart, name, newItem);
   return newCart;
 }
@@ -31,3 +31,12 @@ function objectSet(object, key, value) {
   copy[key] = value;
   return copy;
 }
+
+function setFieldByName(cart, name, value) {
+  const item = cart[name];
+  const newItem = objectSet(item, name, value);
+  return newItem;
+}
+/**
+ * field의 이름을 일급으로 만들어서 인자로 받아 처리했다.
+ */
